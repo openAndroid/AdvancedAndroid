a54f3610358f61f47643393968b6753e7b39ffc4
shuaizhimin <shuaizhimin@weibangong.com>
2016-09-03 19:22:32 +0800

    增加UImodule

---
+++ uicomp/src/main/java/com/advance/android/uicomp/guideview/AAGuide.java
@@ -0,0 +1,12 @@
+package com.advance.android.uicomp.guideview;
+
+/**
+ * ——————————————————————————————————
+ * 作者: shuaizhimin www.shuaizm.com
+ * 描述:
+ * 日期: 2016-09-03
+ * 时间: 17:51
+ * ——————————————————————————————————
+ */
+public class AAGuide {
+}
