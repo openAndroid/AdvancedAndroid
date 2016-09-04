e4dbabb7a86d9b9bc856a0420501e0a8fe0a9103
shuaizhimin <shuaizhimin@weibangong.com>
2016-09-04 16:44:28 +0800

    增加菜单

---
+++ app/src/main/java/com/advance/android/ui/setting/SettingFragment.java
@@ -0,0 +1,12 @@
+package com.advance.android.ui.setting;
+
+/**
+ * ——————————————————————————————————
+ * 作者: shuaizhimin www.shuaizm.com
+ * 描述:
+ * 日期: 2016-09-04
+ * 时间: 12:43
+ * ——————————————————————————————————
+ */
+public class SettingFragment {
+}
