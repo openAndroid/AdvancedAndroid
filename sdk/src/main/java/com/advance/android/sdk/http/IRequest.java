d5bb9330c20434ff1bf50ab455e907bb47ddb67d
shuaizhimin <shuaizhimin@weibangong.com>
2016-09-27 10:28:44 +0800

    修改

--- sdk/src/main/java/com/advance/android/sdk/http/IRequest.java
+++ sdk/src/main/java/com/advance/android/sdk/http/IRequest.java
@@ -1,11 +1,5 @@
 package com.advance.android.sdk.http;
 
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Request;
-import okhttp3.Response;
 
 /**
  * 作者: shuaizhimin
