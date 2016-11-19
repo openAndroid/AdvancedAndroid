5595aacae4c87703a8410b0b88261936da943bda
shuaizhimin <shuaizhimin@weibangong.com>
2016-11-19 15:26:44 +0800

    增加权限校验

---
+++ app/src/main/java/com/advance/android/test/PemissionTestActivity.java
@@ -0,0 +1,13 @@
+package com.advance.android.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PemissionTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pemission_test);
+    }
+}
---
+++ sdk/src/main/java/com/advance/android/sdk/utils/AAPermissionUtil.java
@@ -0,0 +1,24 @@
+package com.advance.android.sdk.utils;
+
+import android.Manifest;
+
+/**
+ * 作者: shuaizhimin
+ * 描述: 权限校验
+ * 日期: 2016-11-18
+ * 时间: 16:17
+ * 版本:
+ */
+public class AAPermissionUtil {
+    //危险权限组
+
+
+    public static void checkCalendar(){
+
+
+    }
+
+
+
+
+}
